a4ea8910c783fbbf9b4d83f31264271a5cea7191
yunhao chen <w0847446@myscc.ca>
2026-09-21 17:18:34 -0400

    Stop tracking generated Next.js build backups

--- .next-predeploy-20260916/static/chunks/app/manager/settings/page-4f96c436cc2f74e8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[274,2050],{6051:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[2619,8441,1255,7358],()=>e(e.s=6051)),_N_E=e.O()}]);
